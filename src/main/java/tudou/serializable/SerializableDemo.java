8d3a3345b1c5d053d8b1221887d2d649d1a12614
tudou <m13069200941@163.com>
2016-04-11 14:11:22 +0800

    serializable基础

--- src/main/java/tudou/serializable/SerializableDemo.java
+++ src/main/java/tudou/serializable/SerializableDemo.java
@@ -1,16 +1,35 @@
 package tudou.serializable;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+import org.junit.Test;
+
 public class SerializableDemo implements Serializable {
-	/**
-	* 
-	*/
 	private static final long serialVersionUID = 1L;
+	private final String fileName = "foo.ser";
+	FileOutputStream fs = null;
+	ObjectOutputStream os = null;
+	FileInputStream fis = null;
+	ObjectInputStream ois = null;
+	SerializableDemo myBox;
 	private int width;
 	private int height;
+	@Test
+	public void test() {
+		init();
+		setSerializable(fileName);
+		setDeSerializable(fileName);
+	}
+	private void init() {
+		myBox = new SerializableDemo();
+		myBox.setWidth(50);
+		myBox.setHeight(30);
+	}
 
 	public void setWidth(int width) {
 		this.width = width;
@@ -18,19 +37,51 @@ public class SerializableDemo implements Serializable {
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
-	public static void main(String[] args) {
-		SerializableDemo myBox = new SerializableDemo();
-		myBox.setWidth(50);
-		myBox.setHeight(30);
-
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	// 序列化Serializable
+	private void setSerializable(String fileName) {
 		try {
-			FileOutputStream fs = new FileOutputStream("foo.ser");
-			ObjectOutputStream os = new ObjectOutputStream(fs);
+			fs = new FileOutputStream(fileName);
+			os = new ObjectOutputStream(fs);
 			os.writeObject(myBox);
-			os.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		} finally {
+			try {
+				if (fs != null)
+					fs.close();
+				if (os != null)
+					os.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+	// 反序列化Deserializable
+	private void setDeSerializable(String fileName) {
+		try {
+			fis = new FileInputStream(fileName);
+			ois = new ObjectInputStream(fis);
+			SerializableDemo st1 = (SerializableDemo) ois.readObject();
+			System.out.println("Height = " + st1.getHeight());
+			System.out.println("Width = " + st1.getWidth());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ois != null)
+					ois.close();
+				if (fis != null)
+					fis.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
